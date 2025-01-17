2de107219d7ba076021667acf1d60c80c52ca53e
Kruk Adam <adamkruk.ka@gmail.com>
2025-01-17 14:23:08 +0100

    Update missile and shotting

---
+++ TopDown IIIB/Assets/Scripts/Controllers/MissleController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+namespace Controllers
+{
+    public class MissleController : MonoBehaviour
+    {
+        public void AllahAqba()
+        {
+            Debug.Log("KochamKryptoGejów");
+        }
+    }
+
+}
--- TopDown IIIB/Assets/Scripts/Controllers/ShootingController.cs
+++ TopDown IIIB/Assets/Scripts/Controllers/ShootingController.cs
@@ -4,33 +4,19 @@ namespace Controllers
 {
     public class ShootingController : MonoBehaviour
     {
-        //[SerializeField] private GameObject projectilePrefab; // Prefab pocisku
-        //[SerializeField] private Transform shootPoint; // Punkt, z kt�rego wystrzeliwany jest pocisk
-        //[SerializeField] private float projectileSpeed = 10f; // Pr�dko�� pocisku
-
-        // Update is called once per frame
-        //void Update()
-        //{
-        //    if (Input.GetButtonDown("Fire1")) // Fire1 domy�lnie przypisany do lewego przycisku myszy
-        //    {
-        //        Shoot();
-        //    }
-        //}
+        [SerializeField] private MissleController misslePrefab;
+        [SerializeField] private Transform spawnPoint;
 
         public void Shoot()
         {
-            //// Stw�rz pocisk w punkcie wystrza�u
-            //GameObject projectile = Instantiate(projectilePrefab, shootPoint.position, shootPoint.rotation);
+            if (misslePrefab == null || spawnPoint == null)
+            {
+                Debug.LogError("Spawn point or misslePrefab not set!");
+                return; 
+            }
 
-            //// Nadaj pociskowi pr�dko�� w kierunku, w kt�rym jest obr�cony
-            //Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-            //if (rb != null)
-            //{
-            //    rb.linearVelocity = shootPoint.right * projectileSpeed;
-            //}
-
-
-            Debug.Log("shoot!");
+            var missle = Instantiate(misslePrefab, spawnPoint.position, spawnPoint.rotation);
+            missle.AllahAqba();
         }
     }
 }
